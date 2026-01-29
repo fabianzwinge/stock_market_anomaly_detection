cfaecaf3516f71e40e325c8498272724f66a80e6
fzw <fabian.zwinge2@softwareag.com>
2026-01-29 15:36:13 +0100

    Implemented order_quantity_threshold_ anomaly

--- anomaly_detector/main.py
+++ anomaly_detector/main.py
@@ -18,10 +18,22 @@ app = Application(
 input_topic = app.topic(name=os.getenv("input"))
 output_topic = app.topic(name=os.getenv("output"))
 
+order_quantity_threshold = 25000
+
+def check_order_quantity_threshold_anomaly(df):
+    df['order_quantity_threshold_anomaly'] = bool(df['size'] > order_quantity_threshold)
+    return df
+
 def main(): 
-    streaming_nasdaq_stock_data = app.dataframe(input_topic)
-    streaming_nasdaq_stock_data.print()
-    app.run(streaming_nasdaq_stock_data)
+    streaming_stock_data = app.dataframe(input_topic)
+    streaming_stock_data = streaming_stock_data.apply(check_order_quantity_threshold_anomaly)
+    streaming_stock_data = streaming_stock_data.filter(lambda row: row['order_quantity_threshold_anomaly'] == True)
+
+    streaming_stock_data.print()
+
+    streaming_stock_data.to_topic(output_topic)
+
+    app.run()
 
 if __name__ == "__main__":
     main()
